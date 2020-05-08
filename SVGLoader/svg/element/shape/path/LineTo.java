7059686243fc64bc3af6ae69de927ffb83f5a2d6
jonathonbird <jonathonbird4@gmail.com>
2020-05-08 16:32:51 +0200

    final

--- SVGLoader/svg/element/shape/path/LineTo.java
+++ SVGLoader/svg/element/shape/path/LineTo.java
@@ -7,7 +7,7 @@ public class LineTo extends PathOp {
     private double x1;
     private double y1;
 
-    protected String label = "LineTo";
+    protected String label = "L";
     protected boolean absolute;
 
     public LineTo(String label) {
@@ -20,17 +20,18 @@ public class LineTo extends PathOp {
     }
 
     public boolean load(String expr) {
-        if (expr.contains(" x1=")) {
-            final Double result = SVGParser.extractDouble(expr, " x1=")[0];
+        if (expr.contains("L")) {
+            int start = expr.indexOf("L") + 1;
+            int end = expr.substring(start).indexOf(" ")+start;
+            int middle = expr.substring(start, end).indexOf(",")+start;
+            final Double result = Double.valueOf(expr.substring(start, middle));
             if (result != null)
                 x1 = result;
+            final Double result1 = Double.valueOf(expr.substring(middle + 1, end));
+            if (result1 != null) {
+                y1 = result1;
+            }
         }
-        if (expr.contains(" y1=")) {
-            final Double result = SVGParser.extractDouble(expr, " y1=")[0];
-            if (result != null)
-                y1 = result;
-        }
-
         return false;
     }
     public String toString(){
